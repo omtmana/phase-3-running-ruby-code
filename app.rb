a9d98fe8d0051daee2e5109e04d9711cf1344244
Olivia <oliviatmanalastas@gmail.com>
2022-08-08 09:26:08 -0400

    Done with assignment

---
+++ app.rb
@@ -0,0 +1,13 @@
+# puts [1, 2, 3].inspect
+# puts [1, 2, 3]
+# p [4, 5, 6]
+# pp [{id: 1, hello:'World'} , {id:2, Hello:'Ruby'} , {id:3, Hello:'Moon'}]
+# puts 'Hello World'
+# print 'Hello World'
+# p 'Hello World'
+
+# print [{red, orange, yellow, green, blue}]
+
+puts 'Hello World!'
+print 'Pass this test, please.'
+p [1, 2, 3]
